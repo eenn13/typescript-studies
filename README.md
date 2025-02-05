0a2e9bd649c45b9e03aeeedc7902b9c6d43a0e4c
Eren Turkel <110343430+erenturkel13@users.noreply.github.com>
2025-02-05 21:49:17 +0300

    readme

---
+++ README.md
@@ -0,0 +1,1 @@
+ts-node yourfile.ts
--- index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.digitalRoot = void 0;
-var digitalRoot = function (n) {
-    // your code here
-    return 5;
-};
-exports.digitalRoot = digitalRoot;
-console.log((0, exports.digitalRoot)(5));
